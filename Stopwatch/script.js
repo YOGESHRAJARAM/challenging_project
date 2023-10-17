5bfeebf27f6d853dee1aa831648f974131e1eb38
YOGESH RAJARAM <yogiyogeshrajaram@gmail.com>
2023-10-17 12:16:18 +0530

    stopwatch version2 finished

---
+++ Stopwatch/script.js
@@ -0,0 +1,56 @@
+const btnStart=document.querySelector('.start');
+const btnstop=document.querySelector('.stop');
+const btnreset =document.querySelector('.reset');
+
+let hrs=min=sec=ms=0,starttimer;
+
+btnStart.addEventListener('click',()=>{
+    btnStart.classList.add('start-active');
+    btnstop.classList.remove('stop-active');
+
+    starttimer=setInterval(()=>{
+        ms++;
+        if(ms==100){
+            sec++;
+            ms=0;
+        }
+        if(sec==60){
+            min++;
+            sec=0;
+        }
+        if(min==60){
+            hrs++;
+            min=0;
+        }
+        updateDisplay();
+
+    },10)
+   
+})
+
+btnstop.addEventListener('click',()=>{
+ clearInterval(starttimer);
+ updateDisplay();
+ btnStart.classList.remove('start-active');
+ btnstop.classList.add('stop-active');
+})
+
+btnreset.addEventListener('click',()=>{
+    hrs=min=sec=ms=0;
+    clearInterval(starttimer);
+    updateDisplay();
+    btnStart.classList.remove('start-active');
+    btnstop.classList.remove('stop-active');
+})
+
+function updateDisplay(){
+    phrs=hrs<10?'0'+hrs:hrs;
+    pmin=min<10?'0'+min:min;
+    psec=sec<10?'0'+sec:sec;
+    pms=ms<10?'0'+ms:ms;
+    document.querySelector('.hrs').innerText=phrs;
+    document.querySelector('.min').innerText=pmin;
+    document.querySelector('.sec').innerText=psec;
+    document.querySelector('.ms').innerText=pms;
+
+}
